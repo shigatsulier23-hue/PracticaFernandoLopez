c9811e0ef2d0d14c74e61e8bf5117773ad2148d5
shigatsulier23-hue <shigatsulier23@gmail.com>
2026-08-17 16:38:45 -0600

    .

---
+++ PRACTICA0JS/js/script.js
@@ -0,0 +1,23 @@
+
+/*alerta("Hola, soy una Alerta creada por Fernando");
+
+document.write("Pozole pozolito, calientito y picosito");
+
+console.log("Hola soy un mensaje en la consola");
+
+let nom = document.getElementById("jose");
+
+nom.innerHTML = "Hola Rodrigo"*/
+
+
+function comprar() {
+
+let Saldo = 90;
+let Costo =100;
+
+let parrafoAlerta = document.getElementById("alerta");
+parrafoAlerta.innerHTML =("saldo insuficiente");
+
+parrafoAlerta.classList.add("saldo insuficiente")
+}
+
